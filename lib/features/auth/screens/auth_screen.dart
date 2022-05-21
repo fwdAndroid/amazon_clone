b7c3f1cc84777868456db20af306d3da728c154c
fwdAndroid <fwdkaleem@gmail.com>
2022-05-21 13:36:27 +0500

    AUth SCreen UI

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,9 +1,17 @@
+import 'package:amazon_clone/commons/widgets/custom_button.dart';
+import 'package:amazon_clone/commons/widgets/custome_textform_field.dart';
+import 'package:amazon_clone/constants/golobal_variables.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+enum Auth {
+  signin,
+  signup,
+}
+
 class AuthScreen extends StatefulWidget {
-    static const String routeName = '/auth-screen';
+  static const String routeName = '/auth-screen';
 
   const AuthScreen({Key? key}) : super(key: key);
 
@@ -12,10 +20,145 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  
+    Auth _auth = Auth.signup;
+final _signUpFormKey = GlobalKey<FormState>();
+  final _signInFormKey = GlobalKey<FormState>();
+   final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+  }
+
+  // void signUpUser() {
+  //   authService.signUpUser(
+  //     context: context,
+  //     email: _emailController.text,
+  //     password: _passwordController.text,
+  //     name: _nameController.text,
+  //   );
+  // }
+
+  // void signInUser() {
+  //   authService.signInUser(
+  //     context: context,
+  //     email: _emailController.text,
+  //     password: _passwordController.text,
+  //   );
+  // }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-    
+    return Scaffold(
+      backgroundColor: GlobalVariables.backgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Text('Welcome',style: TextStyle(fontSize: 22,fontWeight: FontWeight.w500),),
+              ListTile(
+                title: Text('Create Account',style: TextStyle(fontWeight: FontWeight.bold),),
+                leading: Radio(value: Auth.signup, groupValue: _auth, onChanged: (Auth? val){
+                  setState(() {
+                     _auth = val!;
+                  });
+                },activeColor: GlobalVariables.secondaryColor,),
+              ),
+              if (_auth == Auth.signup)
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signUpFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          controller: _nameController,
+                          hintText: 'Name',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomButton(
+                          text: 'Sign Up',
+                          onTap: () {
+                            if (_signUpFormKey.currentState!.validate()) {
+                              // signUpUser();
+                            }
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ListTile(
+                tileColor: _auth == Auth.signin
+                    ? GlobalVariables.backgroundColor
+                    : GlobalVariables.greyBackgroundCOlor,
+                title: const Text(
+                  'Sign-In.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signin,
+                  groupValue: _auth,
+                  onChanged: (Auth? val) {
+                    setState(() {
+                      _auth = val!;
+                    });
+                  },
+                ),
+              ),
+               if (_auth == Auth.signin)
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.backgroundColor,
+                  child: Form(
+                    key: _signInFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomButton(
+                          text: 'Sign In',
+                          onTap: () {
+                            if (_signInFormKey.currentState!.validate()) {
+                              // signInUser();
+                            }
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
