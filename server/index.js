9dc879c8cd7c8de3d5b4ee3c15b507c482f491e9
fwdAndroid <fwdkaleem@gmail.com>
2022-05-24 01:20:45 +0500

    Make Connection with SignUp Api

--- server/index.js
+++ server/index.js
@@ -3,33 +3,36 @@ const express = require("express");
 const mongoose = require("mongoose");
 // const adminRouter = require("./routes/admin");
 // IMPORTS FROM OTHER FILES
-// const authRouter = require("./routes/auth");
+const authRouter = require("./routes/auth"); 
 // const productRouter = require("./routes/product");
 // const userRouter = require("./routes/user");
 
 // INIT
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const DB =
-//   "mongodb+srv://rivaan:rivaan123@cluster0.wpyhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DB =
+  "mongodb+srv://zeosoft:Zeo123456@cluster0.dzacj.mongodb.net/?retryWrites=true&w=majority  ";
 
 // middleware
-// app.use(express.json());
-// app.use(authRouter);
+// use to communicate between client to server
+// app.use(express.json()); 
+ app.use(authRouter);
 // app.use(adminRouter);
 // app.use(productRouter);
 // app.use(userRouter);
 
 // Connections
-// mongoose
-//   .connect(DB)
-//   .then(() => {
-//     console.log("Connection Successful");
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
 
-app.listen(PORT, "0.0.0.0", () => {
+
+app.listen(PORT, () => {
   console.log(`connected at port ${PORT}`);
 });
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
